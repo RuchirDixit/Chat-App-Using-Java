ceca584423c5f687373d67e34773d1f8aeb64b42
Ruchir-BridgeLabz <ruchir.dixit@bridgelabz.com>
2024-06-12 01:02:49 +0530

    Added Client class

--- src/com/chatapp/Server.java
+++ src/com/chatapp/Server.java
@@ -1,7 +1,43 @@
 package com.chatapp;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.*;
 public class Server {
+
+    ServerSocket serverSocket;
+    Socket socket;
+    BufferedReader bufferedReader;
+    PrintWriter out;
+
+    public Server(){
+        try {
+            serverSocket = new ServerSocket(8887);
+            System.out.println("Server is ready to accept new requests....");
+            socket = serverSocket.accept();
+
+            // get input stream from socket (client) convert bytes into chars using InputStreamReader and assign it to BUfferedReader
+            bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            out = new PrintWriter(socket.getOutputStream());
+
+            startReading();
+            startWriting();
+
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    private void startReading() {
+    }
+
+    private void startWriting() {
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello");
+        System.out.println("Server is starting....");
+        new Server();
     }
 }
